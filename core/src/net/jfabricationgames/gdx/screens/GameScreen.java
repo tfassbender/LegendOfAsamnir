5b2c56ce426a5691b14b25848c9068f3742f9925
tobias <tobias-fassbender@gmx.de>
2020-08-08 19:22:06 +0200

    added a new tile map that was created out of a tile texture, that was created by my image tools (just for test reasons)

--- core/src/net/jfabricationgames/gdx/screens/GameScreen.java
+++ core/src/net/jfabricationgames/gdx/screens/GameScreen.java
@@ -92,8 +92,6 @@ public class GameScreen extends ScreenAdapter {
 		cameraHud = new OrthographicCamera();
 		viewport = new FitViewport(SCENE_WIDTH, SCENE_HEIGHT, camera);
 		viewportHud = new FitViewport(SCENE_WIDTH, SCENE_HEIGHT, cameraHud);
-		//viewport = new ExtendViewport(SCENE_WIDTH, SCENE_HEIGHT, camera);
-		//viewportHud = new ExtendViewport(SCENE_WIDTH, SCENE_HEIGHT, cameraHud);
 		
 		cameraHud.position.x = SCENE_WIDTH * 0.5f;
 		cameraHud.position.y = SCENE_HEIGHT * 0.5f;
@@ -111,7 +109,6 @@ public class GameScreen extends ScreenAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		moveCamera(delta);
-		//cropCameraPosition();
 		renderer.setView(camera);
 		renderer.render();
 		renderDebugGraphics(delta);
@@ -148,16 +145,6 @@ public class GameScreen extends ScreenAdapter {
 		//update the camera to re-calculate the matrices
 		camera.update();
 	}
-	
-	//	private void cropCameraPosition() {
-	//		if (((int) Math.round(camera.position.x)) % 32 == 0) {//tile size is 32
-	//			camera.position.x += 1; 
-	//		}
-	//		if (((int) Math.round(camera.position.y)) % 32 == 0) {
-	//			camera.position.y += 1;
-	//		}
-	//		camera.update();
-	//	}
 	
 	private void renderDebugGraphics(float delta) {
 		debugGridRenderer.updateCamera(camera);
