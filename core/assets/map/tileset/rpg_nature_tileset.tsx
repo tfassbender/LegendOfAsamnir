dfee538ddd6963a43d145455bee9168d04271af5
tobias <tobias-fassbender@gmx.de>
2020-12-28 09:59:46 +0100

    improvements and refactorings of map files

--- core/assets/map/tileset/rpg_nature_tileset.tsx
+++ core/assets/map/tileset/rpg_nature_tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.1" name="rpg_nature_tileset" tilewidth="128" tileheight="128" spacing="8" margin="2" tilecount="180" columns="20">
- <image source="tileset/rpg_nature_tileset.png" width="2848" height="1224"/>
+ <image source="images/rpg_nature_tileset.png" width="2848" height="1224"/>
 </tileset>
