5238a54ef86f574fc0669bed928086b7b70c3576
tobias <tobias-fassbender@gmx.de>
2022-02-19 12:14:50 +0100

    - increased the attack damage of spin attack and jump attack - changed the versino number to 0.3.0

--- core/src/net/jfabricationgames/gdx/constants/Constants.java
+++ core/src/net/jfabricationgames/gdx/constants/Constants.java
@@ -2,7 +2,7 @@ package net.jfabricationgames.gdx.constants;
 
 public class Constants {
 	
-	public static final String GAME_VERSION = "0.2.0";
+	public static final String GAME_VERSION = "0.3.0";
 	
 	public static final boolean DEBUG = false;
 	
